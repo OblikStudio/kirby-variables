64200f811400b50a61f37623c23ca7dd5bd76d66
Hristiyan Dodov <h.dodov@gmail.com>
2019-08-12 10:50:17 +0300

    Add tests setup

---
+++ tests/roots/languages/en.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'code' => 'en',
+    'default' => true,
+    'direction' => 'ltr',
+    'locale' => [
+        'en_US'
+    ],
+    'name' => 'English',
+    'translations' => Oblik\Variables\Handler::load('en'),
+    'url' => NULL
+];
