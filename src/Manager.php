f3ad707bcfb9c0cec00a4212b558222af4a8d114
Hristiyan Dodov <h.dodov@gmail.com>
2019-09-25 10:24:26 +0300

    Add export() and import() in Manager

--- src/Manager.php
+++ src/Manager.php
@@ -45,4 +45,28 @@ class Manager
 
         return $translations;
     }
+
+    public static function export(string $lang)
+    {
+        $handler = self::getHandler($lang);
+        $data = null;
+
+        if ($handler) {
+            $data = $handler->data;
+        }
+
+        return $data;
+    }
+
+    public static function import(string $lang, array $data)
+    {
+        $handler = self::getHandler($lang);
+
+        if ($handler && is_array($handler->data)) {
+            $data = array_replace_recursive($handler->data, $data);
+        }
+
+        $handler->data = $data;
+        $handler->write();
+    }
 }
